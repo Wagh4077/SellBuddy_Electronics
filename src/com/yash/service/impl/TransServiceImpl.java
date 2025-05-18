dba8c946f9c012571e022e5c8e41ace232067624
Yash Wagh <sellbuddy1@gmail.com>
2025-05-18 22:25:15 +0530

    first commit

---
+++ src/com/yash/service/impl/TransServiceImpl.java
@@ -0,0 +1,39 @@
+package com.yash.service.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.yash.service.TransService;
+import com.yash.utility.DBUtil;
+
+public class TransServiceImpl implements TransService {
+
+	@Override
+	public String getUserId(String transId) {
+		String userId = "";
+
+		Connection con = DBUtil.provideConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+
+			ps = con.prepareStatement("select username from transactions where transid=?");
+
+			ps.setString(1, transId);
+
+			rs = ps.executeQuery();
+
+			if (rs.next())
+				userId = rs.getString(1);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return userId;
+	}
+
+}
